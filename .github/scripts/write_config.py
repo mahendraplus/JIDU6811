ca5e488847146a6753774a4f8c99d90e2276cb14
mahendraplus <mahendraplus@users.noreply.github.com>
2026-08-12 16:42:41 +0530

    MaxNet OpenWrt Firmware for Jio AirFiber IDU (JIDU6811)
    IPQ9574 + QCA8075 + QCN9074 board support for OpenWrt
    Created by Mahendra Mali (Max) - mahendraplus.github.io

---
+++ .github/scripts/write_config.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Writes OpenWrt .config for JioRouter AX6000 (JIDU6J11-6811 / JIDU6811) build matching Sandiep PR"""
+import sys
+
+config = [
+    "CONFIG_TARGET_qualcommbe=y",
+    "CONFIG_TARGET_qualcommbe_ipq95xx=y",
+    "CONFIG_TARGET_qualcommbe_ipq95xx_DEVICE_jiorouter_ax6000-jidu6j11-6811=y",
+    "CONFIG_TARGET_qualcommbe_ipq95xx_DEVICE_jio_jidu6811=y",
+    "CONFIG_TARGET_ROOTFS_INITRAMFS=y",
+    "CONFIG_TARGET_INITRAMFS_FORCE=y",
+    "CONFIG_KERNEL_GZIP=y",
+    "CONFIG_CCACHE=y",
+
+    # Essential built-in SoC & Wi-Fi drivers (=y)
+    "CONFIG_PACKAGE_kmod-qcom-gcc-ipq9574=y",
+    "CONFIG_PACKAGE_kmod-qcom-nsscc-ipq9574=y",
+    "CONFIG_PACKAGE_kmod-qcom-ppe=y",
+    "CONFIG_PACKAGE_kmod-ath11k=y",
+    "CONFIG_PACKAGE_kmod-ath11k-pci=y",
+    "CONFIG_PACKAGE_kmod-ath11k-ahb=y",
+
+    # Wireless & network tools in initramfs (=y)
+    "CONFIG_PACKAGE_iw=y",
+    "CONFIG_PACKAGE_iwinfo=y",
+    "CONFIG_PACKAGE_wireless-regdb=y",
+    "CONFIG_PACKAGE_busybox=y",
+    "CONFIG_PACKAGE_dropbear=y",
+    "CONFIG_PACKAGE_mtd=y",
+    "CONFIG_PACKAGE_uboot-envtools=y",
+
+    # Modules for rootfs / sysupgrade
+    "CONFIG_PACKAGE_wpad-basic-mbedtls=m",
+    "CONFIG_PACKAGE_luci=m",
+    "CONFIG_PACKAGE_kmod-usb3=m",
+    "CONFIG_PACKAGE_kmod-usb-dwc3=m",
+    "CONFIG_PACKAGE_kmod-usb-dwc3-qcom=m",
+    "CONFIG_PACKAGE_kmod-leds-gpio=m",
+    "CONFIG_PACKAGE_kmod-gpio-button-hotplug=m",
+    "CONFIG_PACKAGE_kmod-ledtrig-netdev=m",
+]
+
+out = sys.argv[1] if len(sys.argv) > 1 else ".config"
+with open(out, 'w') as f:
+    f.write('\n'.join(config) + '\n')
+
+print("Written", out)
